84fbc67c5571045eb3b8c91df002d3462988c3b9
Ashley Flynn <ashnflynn@gmail.com>
2026-04-17 12:04:05 -0500

    updated package.json and vite.config.js to try to deploy to github

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: "/lvl3-w2-d3-assignment/",
+});
